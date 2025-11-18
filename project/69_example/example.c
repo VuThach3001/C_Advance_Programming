c2354dc33488f61374ef17a2b3ab593477f3b51a
Vu Thach <thach30012@gmail.com>
2025-11-18 08:02:07 +0700

    update: Finish example for video 69

--- project/69_example/example.c
+++ project/69_example/example.c
@@ -17,8 +17,44 @@ int main (void)
     fclose (fp);
     */
     /* OPTION 2 */
-    char ch = 'd';
-    putc (ch, stdout);
+    // char ch = 'd';
+    // putc (ch, stdout);
+
+    /* OPTION 3 */
+    // char string[] = "Hello Thach, \nwhatever!\n";
+    // int i = 0;
+
+    // while (string[i] != '\0')
+    // {
+    //     if (string[i] != '\n')
+    //     {
+    //         putchar (string[i]);
+    //     }
+    //     i++;
+
+    // }
+
+    /* OPTION 3 */
+    // int ch = 0;
+    // while ((ch = getchar ()) != EOF)
+    //     putchar (ch);
+
+    // /* ungetc() returns EOF previously read back to stdin */
+    // ungetc (ch, stdin);
+    // printf ("Thank you!\n");
+
+    /* OPTION 4 */
+    FILE* pFile = NULL;
+    char c = '\0';
+
+    pFile = fopen ("myFile.txt", "w");
+
+    if (pFile != NULL)
+    {
+        for (c = 'A'; c <= 'Z'; c++)
+            fputc (c, stdout);
+        fclose (pFile);
+    }
 
     return 0;
 }
