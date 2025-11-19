4ce2d487f5317117d4c798cd9a2cfb512f42ad73
Vu Thach <thach30012@gmail.com>
2025-11-19 08:43:16 +0700

    wip: create the template for challenge of videio 73

---
+++ project/72_challenge/challenge.c
@@ -0,0 +1,22 @@
+/*
+- This challenge will test your understanding of the common string I/O functions
+    - You must choose the correct function to use based on the requirements
+    (sometimes, there are multiple options)
+- Write a program that takes two command-line arguments
+    - The first is a character
+    - The second is a filename
+- The requirements of the program are that it should print only those lines in
+the file containing the given character
+    - Lines in a file are identified by a terminating '\n'
+    - Assume that no line is more than 256 characters long
+- You are required to use fgets() or getline() (or try both) for reading of the file
+    - Use puts to display the output
+*/
+#include <stdio.h>
+
+int main (void)
+{
+
+
+    return 0;
+}
